bd8f952fe23d82c98f9f79f5475b8d79882ff7bf
123 <92643007+melow-123@users.noreply.github.com>
2021-11-21 16:53:11 +0300

    Add project files.

---
+++ DersTekrar/Program.cs
@@ -0,0 +1,31 @@
+﻿using OgrenciLib;
+using System;
+
+namespace DersTekrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.ad = "Ahmet";
+            ogrenci.soyad = "Mehmet";
+            ogrenci.telno = "123456789";
+            ogrenci.yas = 20;
+            ogrenci.Ortalama = 3;
+            Console.WriteLine(ogrenci.Basaridurum);
+            Console.ReadKey();
+
+
+        }
+
+
+    }
+
+    
+
+
+}
+
+
---
+++ OgrenciLib/Ogrenci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OgrenciLib
+{
+    public class Ogrenci
+    {
+        public string ad;
+        public string soyad;
+        public string telno;
+        public byte yas;
+        double ortalama;
+        string basaridurum;
+
+        public string Basaridurum { get => basaridurum; }
+        public double Ortalama
+        {
+            get
+            {
+                return ortalama;
+            }
+            set
+            {
+                ortalama = value;
+                if (value < 2)
+                {
+                    basaridurum = "basarısız";
+                }
+                else
+                {
+                    basaridurum = "basarılı";
+                }
+            }
+        }
+
+    }
+}
